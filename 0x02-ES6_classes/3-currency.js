627a11824e33419f62e26da1aacfbd4d5104d21f
Mikenziokah <mikenziokah@gmail.com>
2024-03-10 12:39:27 +0000

    currency

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,29 +1,29 @@
 export default class Currency {
-	constructor(code, name) {
-		this._code = code;
-		this._name = name;
-	}
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
 
-	// getter and setter for code
-	get code() {
-		return this._code;
-	}
+  // getter and setter for code
+  get code() {
+    return this._code;
+  }
 
-	set code(newCode) {
-		this._code = newCode;
-	}
+  set code(newCode) {
+    this._code = newCode;
+  }
 
-	// getter and setter for name
-	get name() {
-		return this._name;
-	}
+  // getter and setter for name
+  get name() {
+    return this._name;
+  }
 
-	set name(newName) {
-		this._name = newName;
-	}
+  set name(newName) {
+    this._name = newName;
+  }
 
-	// method of display
-	displayFullCurrency() {
-		return `${this._name} (${this._code})`;
-	}
+  // method of display
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
 }

86147f007b3fab6b7a447b00f9bd018fa7bd43f3
Mikenziokah <mikenziokah@gmail.com>
2024-03-10 13:30:43 +0000

    pricing

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,37 +1,37 @@
-import Currenct from './3-currency'
+// eslint-disable-next-line
+import Currency from './3-currency';
 
 export default class Pricing {
-	constructor(amount, currency) {
-		this._amount = amount;
-		this._currency = currency;
-	}
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
 
-	// getter and setter for amount
-	get amount() {
-		return this._amount;
-	}
+  // getter and setter for amount
+  get amount() {
+    return this._amount;
+  }
 
-	set amount(newAmount) {
-		this._amount = newAmount;
-	}
+  set amount(newAmount) {
+    this._amount = newAmount;
+  }
 
-	// getter and setter for currency
-	get currency() {
-		return this._currency;
-	}
+  // getter and setter for currency
+  get currency() {
+    return this._currency;
+  }
 
-	set currency(newCurrency) {
-		this._currency = newCurrency;
-	}
+  set currency(newCurrency) {
+    this._currency = newCurrency;
+  }
 
-	// methods of display
-	displayFullPrice() {
-		 return `${this._amount} ${this._currency.name} (${this._currency.code})`;
-	}
+  // methods of display
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
 
-	// static conversion method
-	static convertPrice(amount, conversionRate) {
-		return amount * conversionRate;
-	}
+  // static conversion method
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
 }
-

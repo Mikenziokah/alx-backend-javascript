dd2d68f56e8109fefba4933315fc7071f5e13fff
Mikenziokah <mikenziokah@gmail.com>
2024-03-12 19:40:23 +0000

    grade city

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,9 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+	return students
+	.filter(student => student.location === city)
+	.map(student => {
+		const grade1 = newGrades.filter(grade => grade.studentId === student.id);
+		const grade = grade1 ? grade1.grade : 'N/A';
+		return { ...student, grade };
+	});
+}

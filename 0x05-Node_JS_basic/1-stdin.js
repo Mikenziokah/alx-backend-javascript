f3c236de60d522fab33ac8f46102193ebe55ddb5
Mikenziokah <mikenziokah@gmail.com>
2024-04-29 19:37:00 +0000

    stdou

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,16 +1,10 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
-process.stdin.setEncoding('utf8');
-process.stdin.on('data', (input) => {
-  const name = input.trim();
-
-  if (name.length > 0) {
-    process.stdout.write(`Your name is: ${name}\n`);
-  } else {
-    process.stdout.write('Please enter a valid name.\n');
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name !== null) {
+    process.stdout.write(`Your name is: ${name}`);
   }
 });
-
-// Listen for the 'end' event on stdin (when input stream ends)
-process.stdin.on('end', () => {
+process.on('exit', () => {
   process.stdout.write('This important software is now closing\n');
 });

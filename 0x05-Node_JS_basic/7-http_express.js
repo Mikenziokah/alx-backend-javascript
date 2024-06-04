127160c575e6ae8e1876be3deac185214da21895
Mikenziokah <mikenziokah@gmail.com>
2024-06-04 20:22:22 +0300

    7http

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -3,6 +3,7 @@ const countStudents = require('./3-read_file_async');
 
 const app = express();
 app.get('/', (req, res) => res.send('Hello Holberton School!'));
+
 app.get('/students', async (req, res) => {
   const title = 'This is the list of our students\n';
   try {

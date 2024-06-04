61053f5f9456d97dc673cddbcfeb6095aa72636f
Mikenziokah <mikenziokah@gmail.com>
2024-06-04 20:30:49 +0300

    6express

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -6,5 +6,8 @@ app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
-app.listen(1245);
+app.listen(1245, () => {
+  console.log('Server is listening on port 1245');
+});
+
 module.exports = app;
